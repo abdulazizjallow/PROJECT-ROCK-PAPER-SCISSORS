9934a529598022cdb631bc36ecc581757ce9b373
abdulazizjallow <abdulazizjallow96@gmail.com>
2022-05-16 14:17:57 +0200

    adding my computerselection function

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 'use strict'
+const choice = ['rock', 'paper', 'scissors'];
+
+const computerPlay = function (chioce) {
+    return choice[Math.floor(Math.random()*choice.length)];
+}
+console.log(computerPlay())
